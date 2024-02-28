477bc2874f9cb41983fbf333504c4f141a500e34
ForBai <thelars42@gmail.com>
2024-02-28 18:12:52 +0100

    some stuff

--- src/main/java/me/forbai/Main.java
+++ src/main/java/me/forbai/Main.java
@@ -1,29 +1,51 @@
 package me.forbai;
 
-import java.io.*;
-import java.util.Base64;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
-    public static String hwid;
-    public static String correctHwid = "none";
-    public static boolean isHwidCorrect = false;
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Getting HWID for license");
-        File file = new File("/proc/cpuinfo");
-        FileInputStream fileIn = new FileInputStream(file);
-        hwid = Base64.getEncoder().encodeToString(fileIn.readAllBytes());
-        Thread setThread = new Thread(() -> {
-            isHwidCorrect = hwid.equals(correctHwid);
-        }, "check-hwid-thread");
-        setThread.start();
-        if (isHwidCorrect) {
-            System.out.println("You can now do something funny!");
-
-        } else {
-            System.out.println("ERRRRR Wrong HWID, Please verify yor hwid!");
-            System.exit(-1);
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        JDialog dialog = createDialog();
+        dialog.setVisible(true);
     }
+
+    private static JDialog createDialog() {
+        JDialog dialog = new JDialog();
+        dialog.setModal(true);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setSize(400, 200);
+
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Enter your license key:"));
+        JTextField textField = new JTextField(20);
+        panel.add(textField);
+        panel.add(createSubmitButton(textField, dialog));
+
+        dialog.add(panel);
+        return dialog;
+    }
+
+    private static JButton createSubmitButton(JTextField textField, JDialog dialog) {
+        JButton button = new JButton("Submit");
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+        button.setBackground(Color.GREEN);
+        button.addActionListener(e -> {
+            if (textField.getText().equals("1234")) {
+                dialog.dispose();
+                JOptionPane.showMessageDialog(null, "Correct license key!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Incorrect license key!");
+            }
+        });
+        return button;
+    }
+
 }

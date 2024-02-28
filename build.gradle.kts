31b80473f131da0fedfcbc62e84e1725c214d358
forbai <thelars42@gmail.com>
2024-02-28 16:51:55 +0100

    t

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("java")
 }
 
@@ -7,13 +10,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.getByName<ShadowJar>("shadowJar") {
+    manifest {
+        attributes(
+            "Main-Class" to "me.forbai.Main"
+        )
+    }
 }
--- src/main/java/me/forbai/Main.java
+++ src/main/java/me/forbai/Main.java
@@ -1,8 +1,29 @@
 package me.forbai;
 
+import java.io.*;
+import java.util.Base64;
+
 public class Main {
-    public static void main(String[] args) {
 
+    public static String hwid;
+    public static String correctHwid = "none";
+    public static boolean isHwidCorrect = false;
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Getting HWID for license");
+        File file = new File("/proc/cpuinfo");
+        FileInputStream fileIn = new FileInputStream(file);
+        hwid = Base64.getEncoder().encodeToString(fileIn.readAllBytes());
+        Thread setThread = new Thread(() -> {
+            isHwidCorrect = hwid.equals(correctHwid);
+        }, "check-hwid-thread");
+        setThread.start();
+        if (isHwidCorrect) {
+            System.out.println("You can now do something funny!");
+
+        } else {
+            System.out.println("ERRRRR Wrong HWID, Please verify yor hwid!");
+            System.exit(-1);
+        }
     }
 }

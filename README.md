988e3ac61a7ff70b67358108117b5e49b22c6519
ForBai <thelars42@gmail.com>
2024-02-28 21:42:55 +0100

    yea

---
+++ README.md
@@ -0,0 +1,34 @@
+# Java Reverse Engineering For Beginners
+
+This repository is a beginner's guide to reverse engineering in Java. It contains a variety of examples and exercises to help you understand and practice the concepts of reverse engineering.
+
+## Building the Project
+
+This project uses the Gradle build system. To build the project, follow these steps:
+
+1. Ensure you have Gradle installed on your system. If not, you can download it from the [official Gradle website](https://gradle.org/install/).
+2. Open a terminal and navigate to the root directory of the project.
+3. Run the following command to build the project:
+
+```bash
+gradle build
+```
+
+This will compile the source code and run the tests. If the build is successful, the compiled classes will be placed in the `build/classes` directory, and a JAR file will be created in the `build/libs` directory.
+
+## Help and References
+
+If you're new to Java or Kotlin, or if you're not familiar with some of the concepts used in this project, here are some resources that might help:
+
+- [Oracle's Java Tutorials](https://docs.oracle.com/javase/tutorial/)
+- [Gradle User Manual](https://docs.gradle.org/current/userguide/userguide.html)
+
+And also here is a list of tools and libraries that can be useful while reverse engineering Java applications:
+- [JD-GUI](http://jd.benow.ca/)
+- [Bytecode Viewer](https://bytecodeviewer.com/)
+- [Javassist](https://www.javassist.org/)
+- [ASM](https://asm.ow2.io/)
+- [ByteBuddy](https://bytebuddy.net/)
+- [Recaf](https://www.cuchazinteractive.com/recaf.html)
+
+Please note that reverse engineering can be a complex topic, and it's important to use these skills responsibly and ethically. Always respect copyright laws and the terms of use of any software you're working with.
--- src/main/java/me/forbai/Main.java
+++ src/main/java/me/forbai/Main.java
@@ -1,18 +1,28 @@
 package me.forbai;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.HttpsURLConnection;
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class Main {
 
-
     public static void main(String[] args) {
+        System.out.println(getHWID());
+        System.out.println(decrypt("kw9GDVdlzrAzti65rjjNOMYXX76ViwnXtkzqafUjsXBJ69QtRD0qm1FrQDxyUFq/mgH8anOVYBUd7k+n9puycmpJOo6FVYn8cQODCAZ02Mc=", String.valueOf(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath().hashCode())));
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         JDialog dialog = createDialog();
         dialog.setVisible(true);
     }
@@ -33,19 +43,75 @@ public class Main {
         return dialog;
     }
 
+    private static String encrypt(String strToEncrypt, String secret) {
+        try {
+            byte[] key = secret.getBytes(StandardCharsets.UTF_8);
+            key = Arrays.copyOf(key, 16); // use only first 128 bit
+            Key secretKey = new SecretKeySpec(key, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    private static String decrypt(String strToDecrypt, String secret) {
+        try {
+            byte[] key = secret.getBytes(StandardCharsets.UTF_8);
+            key = Arrays.copyOf(key, 16); // use only first 128 bit
+            Key secretKey = new SecretKeySpec(key, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e);
+        }
+        return null;
+    }
+
+    private static String getHWID() {
+        return String.valueOf((System.getProperty("os.name") +
+                System.getProperty("os.version") +
+                System.getProperty("os.arch") +
+                System.getProperty("user.home") +
+                System.getProperty("java.version") +
+                System.getProperty("java.vendor") +
+                System.getenv("PROCESSOR_IDENTIFIER") +
+                System.getenv("COMPUTERNAME") +
+                System.getProperty("user.name")).hashCode());
+    }
+
     private static JButton createSubmitButton(JTextField textField, JDialog dialog) {
         JButton button = new JButton("Submit");
         button.setFont(new Font("Arial", Font.BOLD, 14));
-        button.setBackground(Color.GREEN);
+        button.setBackground(Color.GRAY);
         button.addActionListener(e -> {
-            if (textField.getText().equals("1234")) {
-                dialog.dispose();
-                JOptionPane.showMessageDialog(null, "Correct license key!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Incorrect license key!");
+            try {
+                URL url = new URL("https://raw.githubusercontent.com/ForBai/JavaReversingForBeginners/main/wantedkeyfromurl.txt");
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String[] splitKey = reader.readLine().split("-");
+                reader.close();
+                System.out.println(Arrays.toString(splitKey));
+                String encryptionToken = splitKey[0];
+                String hwid = splitKey[1].replaceAll("\"", "");
+                String licenseKey = splitKey[2];
+
+                if (textField.getText().equals(decrypt(licenseKey, encryptionToken))) {
+                    dialog.dispose();
+                    JOptionPane.showMessageDialog(null, "Correct license key!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Incorrect license key!");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.exit(-1);
             }
         });
         return button;
     }
+
 
 }
